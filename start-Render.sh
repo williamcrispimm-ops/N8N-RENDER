c160cef8939998cd12dd8da15abc4630ce362ac4
williamcrispimm-ops <william.crispimm@gmail.com>
2025-09-02 13:37:00 -0300

    Update start-Render.sh

--- start-Render.sh
+++ start-Render.sh
@@ -1,12 +1,8 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/sh
+# Script para inicializar o n8n no Render
 
-# Se o Render fornecer PORT, usamos como porta do n8n
-export N8N_PORT="${PORT:-5678}"
+# Garante que o Render use a porta correta
+export N8N_PORT=$PORT
 
-# Garante diretório de dados (persistir no Render Disk montado em /home/node/.n8n)
-mkdir -p /home/node/.n8n
-chown -R node:node /home/node/.n8n
-
-# Sobe o n8n (entrypoint oficial cuida do resto)
-exec /docker-entrypoint.sh n8n
+# Inicia o n8n
+n8n start
