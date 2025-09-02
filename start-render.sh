03fd714ebcec5929023ff7cb9381c2a301ac1551
williamcrispimm-ops <william.crispimm@gmail.com>
2025-09-02 17:12:37 -0300

    Update start-render.sh

--- start-render.sh
+++ start-render.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "🚀 Iniciando n8n no Render..."
 
-# Mapear porta do Render -> n8n
-export N8N_PORT=${PORT}
+# Mapear porta do Render para a usada pelo n8n
+export N8N_PORT=$PORT
 
-# Subir n8n
-n8n start
+# Iniciar n8n
+exec n8n start
